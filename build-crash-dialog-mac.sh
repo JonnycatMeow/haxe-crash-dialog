0000aede90433630dc132f951b84c429a4d5c042
jonnyboy <jonnygomez156@gmail.com>
2022-12-30 22:03:52 -0800

    yeeeeee fixed

--- build-crash-dialog-mac.sh
+++ build-crash-dialog-mac.sh
@@ -1,4 +1,4 @@
 cd crash-dialog
 haxe hxwidgets-mac.hxml  
-cp build/macos/Main ../export/mac/IzzyEngine-CrashDialog
+cp build/macos/Main ../export/release/macos/bin/IzzyEngine-CrashDialog
 cd ..
--- build-crash-dialog-windows.bat
+++ build-crash-dialog-windows.bat
@@ -1,6 +1,6 @@
 @echo off 
 cd crash-dialog
 haxe hxwidgets-windows.hxml
-copy build\windows\Main.exe ..\export\windows\IzzyEngine-CrashDialog.exe
+copy build\windows\Main.exe ..\export\release\windows\bin\IzzyEngine-CrashDialog.exe
 cd ..
 @echo on
