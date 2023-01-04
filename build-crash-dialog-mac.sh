12862cd4540d48b35acb889f1a14ec81c375f96f
johnny <107599365+JonnycatMeow@users.noreply.github.com>
2023-01-03 17:12:42 -0800

    Update build-crash-dialog-mac.sh

--- build-crash-dialog-mac.sh
+++ build-crash-dialog-mac.sh
@@ -1,4 +1,4 @@
 cd crash-dialog
 haxe hxwidgets-mac.hxml  
-cp build/macos/Main ../export/release/macos/bin/IzzyEngine-CrashDialog
+cp build/macos/Main.app ../export/release/macos/bin/IzzyEngine-CrashDialog.app
 cd ..
